76c34fb592e888fdb0e8143c84d4d6e4c973da7b
henryjindal112 <148120479+henryjindal112@users.noreply.github.com>
2024-02-10 15:12:27 +0530

    Update constants.js

--- interface/src/constants.js
+++ interface/src/constants.js
@@ -1,116 +1,18 @@
 export const NFTS_LIST = ["BAYC", "WOW", "AZUKI"]
 
 export const NFT_CONTRACTS = {
-    "9747778": {
-        "rpc": "http://38.242.129.140:8545",
-        "fluidAddress": "0xF47C02C685AD9C5a16afB1c164F7CDA0da521507",
-        "fluidity": "0x555131fBa03bEA1aB1a999DA27eA36415d05dddb",
+    "1432979": {
+        "rpc": "https://d3.acethelight.xyz",
+        "fluidAddress": "0xb42cbCAef397e7333ab446d34b19dff91dE7a165",
+        "fluidity": "0xCF72C8157612A2625B335747A2F59C77f9E87d0D",
         "BAYC": {
-            contractAddress: "0x53b80ac690C37458b4396992A5cE5c6F25a6D0D2"
+            contractAddress: "0x8cacf26B4D86955d60f8345b1986e59431D84F19"
         },
         "WOW": {
-            contractAddress: "0x6d187159784A767108C41b7212E67Be9BEdaCD86"
+            contractAddress: "0x8CEC608A796e5c1cD8d1f420D0B733F534604886"
         },
         "AZUKI": {
-            contractAddress: "0xDFD4B28346824d56c7f3880F067eeD040BffBB1B"
-        }
-    },
-    "9193196": {
-        "rpc": "https://froopyland.dymension.xyz/13/arkham_9193196-1/evmrpc",
-        "fluidAddress": "0x77325bF80225B7FDc1D7AdF3451db5A04A0973BB",
-        "fluidity": "0xD4441eCbC073970c86a6C121c47f371D1d109ceA",
-        "BAYC": {
-            contractAddress: "0x955B4c00E2085E00dbfC78E7FfE2c588C570bBdA"
-        },
-        "WOW": {
-            contractAddress: "0x1367082758BCaAeE041992d56B53CbAdEB477079"
-        },
-        "AZUKI": {
-            contractAddress: "0x439BB432207B70496272a333A6D9F419156D1340"
-        }
-    },
-    "2177968": {
-        "rpc": "https://froopyland.dymension.xyz/14/avocado_2177968-1/evmrpc",
-        "fluidAddress": "0x7bA861BaB92aeCbb10f6d10526a65157234892e6",
-        "fluidity": "0x8075aF16CC7fb79A2a88fC9Cfb515D3A4717D1bd",
-        "BAYC": {
-            contractAddress: "0x1b0F8a42f923AFB3192C7989A65d746d0011f226"
-        },
-        "WOW": {
-            contractAddress: "0x223DD4973aD7C200b68c28afFE9BD23A63286a31"
-        },
-        "AZUKI": {
-            contractAddress: "0xBb56f7494620D8C975279bA2798b0F28Ad7eF478"
-        }
-    },
-    "6468241": {
-        "rpc": "https://froopyland.dymension.xyz/14/animus_6468241-1/evmrpc",
-        "fluidAddress": "0x66A2421da71B68E6c5575a3bfC73aB8c5ACc81e1",
-        "fluidity": "0x338C9071a1c4BaaA2444fd0De8803910CC33b480",
-        "BAYC": {
-            contractAddress: "0x49cAfc6A37e5Fb53D36AF4Fd9261B5651C70e910"
-        },
-        "WOW": {
-            contractAddress: "0x366F6B52581997A219cbdc821A55FAd42AA76859"
-        },
-        "AZUKI": {
-            contractAddress: "0xcCF9B959C0f09221290fcf91418BB3f375AB1F6E"
-        }
-    },
-    "5736254": {
-        "rpc": "http://161.97.107.208:8545",
-        "fluidAddress": "0x59621fab59267E204252F051Da2ecb82104dFd0F",
-        "fluidity": "0x772990E45c62EC246A2CFa321f97fC1677b4fBbb",
-        "BAYC": {
-            contractAddress: "0x28326CCE8A1963b09485567528998afa1Db313A8"
-        },
-        "WOW": {
-            contractAddress: "0xa33f71D06353c54af32f748112c08C754d3A7df3"
-        },
-        "AZUKI": {
-            contractAddress: "0x34cC6F17A99C202c6e98148D9fad02AB541A402c"
-        }
-    },
-    "2827710": {
-        "rpc": "https://froopyland.dymension.xyz/14/devilmaycry_2827710-1/evmrpc",
-        "fluidAddress": "0xf1203E47EE2874B0d7C134fD27Fc3F4F22Ea4fA3",
-        "fluidity": "0x20CE8B6aEB134da17655b8C8f5DFE0C5a8e83a0c",
-        "BAYC": {
-            contractAddress: "0x4699Af7ac5e7F2E3bF8FEc6c6288fDDFb189D70d"
-        },
-        "WOW": {
-            contractAddress: "0x76aBCcEB75ff82e5c4Ca1E515FB01d29065A7809"
-        },
-        "AZUKI": {
-            contractAddress: "0xdA14ba1f96B5BBaAC7bF063b66E45f553103A19A"
-        }
-    },
-    "1188624": {
-        "rpc": "https://froopyland.dymension.xyz/14/umbros_1188624-1/evmrpc",
-        "fluidAddress": "0xD4441eCbC073970c86a6C121c47f371D1d109ceA",
-        "fluidity": "0x17E695CA08DaBa6f9b351008A2DD9D65cb8F679b",
-        "BAYC": {
-            contractAddress: "0x1367082758BCaAeE041992d56B53CbAdEB477079"
-        },
-        "WOW": {
-            contractAddress: "0x77325bF80225B7FDc1D7AdF3451db5A04A0973BB"
-        },
-        "AZUKI": {
-            contractAddress: "0x955B4c00E2085E00dbfC78E7FfE2c588C570bBdA"
+            contractAddress: "0xE1F6bc5F092e60077bf81E6B5F76e6fE8D58e045"
         }
     }
-    // "9955692": {
-    //     "rpc": "https://froopyland.dymension.xyz/14/phoenix_9955692-1/evmrpc",
-    //     "fluidAddress": "0xD4441eCbC073970c86a6C121c47f371D1d109ceA",
-    //     "fluidity": "0x17E695CA08DaBa6f9b351008A2DD9D65cb8F679b",
-    //     "BAYC": {
-    //         contractAddress: "0x1367082758BCaAeE041992d56B53CbAdEB477079"
-    //     },
-    //     "WOW": {
-    //         contractAddress: "0x77325bF80225B7FDc1D7AdF3451db5A04A0973BB"
-    //     },
-    //     "AZUKI": {
-    //         contractAddress: "0x955B4c00E2085E00dbfC78E7FfE2c588C570bBdA"
-    //     }
-    // }
 }
